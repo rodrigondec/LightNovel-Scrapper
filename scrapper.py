4656ee03c87e3c6f73d8e81d1841d09bf9a56041
RodrigoNdeC <rodrigondec@gmail.com>
2018-03-23 15:14:52 -0300

    Codigo refatorado em classe

--- scrapper.py
+++ scrapper.py
@@ -1,74 +1,123 @@
 import requests
 from bs4 import BeautifulSoup
 from queue import Queue
+import argparse
+from time import sleep
 
-qt_chapters = 50
-base_url = 'http://www.wuxiaworld.com'
-chapter_queue = Queue()
-chapter_queue.put('/novel/coiling-dragon/cd-book-6-chapter-5')
-book_title = 'Coiling Dragon'
 
-chapters = []
+class Scrapper():
+    base_url = 'http://www.wuxiaworld.com'
 
-for x in range(0, qt_chapters):
-    chapter_url = chapter_queue.get()
-    print("URL: " + base_url + chapter_url)
-    page = requests.get(base_url + chapter_url)
+    def __init__(self):
+        ap = argparse.ArgumentParser()
+        ap.add_argument('-u', '--url', default='/novel/coiling-dragon/cd-book-6-chapter-5', help='Index of first wanted chapter')
+        ap.add_argument('-qc', '--chapters', default=50, type=int, help='Quantity of chapters wanted')
+        ap.add_argument('-t', '--title', default='Coiling Dragon', help='Title of the light novel')
+        ap.add_argument('-d', '--delay', default=1, type=int, help=('Delay between scraping chapters (don\'t wanna '
+                                                                    'get banned!)'))
+        ap.add_argument('-v', '--verbose', default=True, action='store_true', help='Adds debugging statements to outpu')
+        args = ap.parse_args()
 
-    soup = BeautifulSoup(page.content, 'html.parser')
+        self.chapter_queue = Queue()
 
-    # print(soup)
+        self.book_title = args.title
+        self.chapter_queue.put(args.url)
+        self.qt_chapters = args.chapters
+        self.delay = args.delay
+        self.debug = args.verbose
 
-    script = soup.find_all("script")[3]
-    values = script.get_text().replace(' ', '').replace('\n', '').replace('var', '').split(';')
-    next_chapter_url = None
-    for value in values:
-        data = value.split('=')
-        if data[0] == 'NEXT_CHAPTER':
-            next_chapter_url = data[1]
-            next_chapter_url = next_chapter_url.replace('\'', '')
-            print("next url: " + next_chapter_url)
-    chapter_queue.put(next_chapter_url)
+        self.chapters = []
 
-    paragraphs = soup.find_all('p')
-    chapter = []
-    for paragraph in paragraphs:
-        if paragraph.get_text() != '' and paragraph.get_text() != 'Previous Chapter' and \
-                paragraph.get_text() != 'Facebook' and paragraph.get_text() != 'Discord' and \
-                paragraph.get_text() != 'RSS' and paragraph.get_text() != 'Twitter' and \
-                paragraph.get_text() != 'Contact Us' and paragraph.get_text() != 'Privacy Policy' and \
-                paragraph.get_text() != 'Copyright © 2018 WuxiaWorld. All rights reserved.':
-            chapter.append(paragraph)
-    print("Chapter done.")
-    chapters.append(chapter)
-print(chapters)
+        self.current_soup = None
 
-book = open(book_title+'.html', 'w')
-book.write('<!doctype html><html lang="en"><head><meta charset="utf-8" />'
-           '<title>'+book_title+'</title>'
-           '<style>p { margin-top: 1em; text-indent: 0em; } h1 {margin-top: 1em; text-align: center} h2 {margin: 2em 0 1em; text-align: center; font-size: 2.5em;} h3 {margin: 0 0 2em; font-weight: normal; text-align:center; font-size: 1.5em; font-style: italic;} .center { text-align: center; } .pagebreak { page-break-before: always; }</style></head><body>')
-book.write('<h1>'+book_title+'</h1><div class="pagebreak"></div>')
-book.write('<div id="toc">'
-           '<h2>'
-           'Table of Contents <br />'
-           '</h2>'
-           '<ul>')
+    def get_current_soup(self):
+        chapter_url = self.chapter_queue.get()
+        if self.debug:
+            print('Geting soup for link: {}...'.format(Scrapper.base_url + chapter_url))
+        page = requests.get(Scrapper.base_url + chapter_url)
 
-for index in range(0, len(chapters)):
-    book.write('<li><a href="#ch'+str(index+1)+'">'+chapters[index][0].get_text()+'</a></li>')
-book.write('</ul></div><div class="pagebreak"></div>')
+        self.current_soup = BeautifulSoup(page.content, 'html.parser')
+        if self.debug:
+            print('Current soup done.')
+        self.get_next_link()
 
-index = 0
-for chapter in chapters:
-    book.write('<h3 id="ch'+str(index)+'">')
-    book.write(chapter[0].get_text())
-    book.write('</h3>')
-    index += 1
-    for paragraph in chapter[1:]:
-        book.write('<p>')
-        book.write(paragraph.get_text())
-        book.write('</p>')
-    book.write('<div class="pagebreak"></div>')
-book.write('</body>'
-           '</html>')
-book.close()
+    def get_next_link(self):
+        if self.debug:
+            print('Geting link of the next chapter...')
+        script = self.current_soup.find_all("script")[3]
+        values = script.get_text().replace(' ', '').replace('\n', '').replace('var', '').split(';')
+        next_chapter_url = None
+        for value in values:
+            data = value.split('=')
+            if data[0] == 'NEXT_CHAPTER':
+                next_chapter_url = data[1]
+                next_chapter_url = next_chapter_url.replace('\'', '')
+        self.chapter_queue.put(next_chapter_url)
+        if self.debug:
+            print('Next chapter link: {}.'.format(next_chapter_url))
+
+    def proccess_soup(self):
+        if self.debug:
+            print('Processing current soup...')
+        paragraphs = self.current_soup.find_all('p')
+        chapter = {}
+        chapter['title'] = paragraphs[0].get_text()
+        chapter['content'] = []
+        for paragraph in paragraphs[1:]:
+            if paragraph.get_text() != '' and paragraph.get_text() != 'Previous Chapter' and \
+                    paragraph.get_text() != 'Facebook' and paragraph.get_text() != 'Discord' and \
+                    paragraph.get_text() != 'RSS' and paragraph.get_text() != 'Twitter' and \
+                    paragraph.get_text() != 'Contact Us' and paragraph.get_text() != 'Privacy Policy' and \
+                    paragraph.get_text() != 'Copyright © 2018 WuxiaWorld. All rights reserved.':
+                chapter['content'].append(paragraph)
+        if self.debug:
+            print("Process of chapter {} done.".format(chapter['title']))
+        self.chapters.append(chapter)
+
+    def save_file(self):
+        if self.debug:
+            print("Saving chapters to html file...")
+        file = open('{}.html'.format(self.book_title), 'w')
+        file.write('<!doctype html><html lang="en"><head><meta charset="utf-8" />')
+        file.write('<title>{}</title>'.format(self.book_title))
+        file.write('<style>p { margin-top: 1em; text-indent: 0em; } h1 {margin-top: 1em; text-align: center} h2 {margin: 2em 0 1em; text-align: center; font-size: 2.5em;} h3 {margin: 0 0 2em; font-weight: normal; text-align:center; font-size: 1.5em; font-style: italic;} .center { text-align: center; } .pagebreak { page-break-before: always; }</style>')
+        file.write('</head><body>')
+        file.write('<h1>{}</h1><div class="pagebreak"></div>'.format(self.book_title))
+        file.write('<div id="toc">'
+                   '<h2>'
+                   'Table of Contents <br />'
+                   '</h2>'
+                   '<ul>')
+
+        for index in range(0, len(self.chapters)):
+            file.write('<li><a href="#ch{index}">{title}</a></li>'.format(index=index+1, title=self.chapters[index]['title']))
+        file.write('</ul></div><div class="pagebreak"></div>')
+
+        index = 0
+        for chapter in self.chapters:
+            file.write('<h3 id="ch{}">'.format(index+1))
+            file.write(chapter['title'])
+            file.write('</h3>')
+            index += 1
+            for paragraph in chapter['content']:
+                file.write('<p>')
+                file.write(paragraph.get_text())
+                file.write('</p>')
+            file.write('<div class="pagebreak"></div>')
+        file.write('</body>'
+                   '</html>')
+        file.close()
+        if self.debug:
+            print('File html saved.')
+
+    def start(self):
+        if self.debug:
+            print('Scrapper initializing...')
+        for x in range(0, self.qt_chapters):
+            self.get_current_soup()
+            self.proccess_soup()
+            sleep(self.delay)
+        self.save_file()
+
+if __name__ == '__main__':
+    Scrapper().start()
