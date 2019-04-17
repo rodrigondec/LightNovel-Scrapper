9089faacf3bd3c904602781019b499192809fc1c
RodrigoNdeC <rodrigondec@gmail.com>
2019-04-16 23:15:20 -0300

    run on battle

--- run.py
+++ run.py
@@ -2,13 +2,7 @@ from novel import Novel
 
 if __name__ == '__main__':
     Novel.load_novels()
-    c = Novel.get_novel('Archfiend')
+    c = Novel.get_novel("Battle Through the Heavens")
     assert isinstance(c, Novel)
-    # c.add_chosen_book("13")
     c.add_chosen_book("1")
-    # c.add_chosen_book("15")
-    # c.add_chosen_book("16")
-    # c.add_chosen_book("17")
-    # c.add_chosen_book("18")
-    # c.add_chosen_book("19")
     c.process()
