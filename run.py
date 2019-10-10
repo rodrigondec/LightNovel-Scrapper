122fbfb59b26164cb4f6f2ea3811d241028f6df5
rodrigondec <rodrigondec@gmail.com>
2019-10-10 16:24:17 -0300

    run & utils updated

--- run.py
+++ run.py
@@ -1,9 +1,22 @@
-from novel import Novel
+from novel.base import Novel
+from novel.wuxiaworld import WuxiaWorldNovel, WuxiaWorldNovelVolumeLess
 
 if __name__ == '__main__':
-    Novel.load_novels()
-    c = Novel.get_novel("Stop Friendly Fire")
-    assert isinstance(c, Novel)
-    for i in range(2, 10):
-        c.add_chosen_book(f"{i}")
-    c.process()
+    title = "Stop Friendly Fire"
+    data = Novel.get_novel_data(title)
+    if data.get('type') == 'wuxiaworld':
+        if data.get('has_books'):
+            novel = WuxiaWorldNovel.from_data(data)
+        else:
+            novel = WuxiaWorldNovelVolumeLess.from_data(data)
+    elif data.get('type') == 'mofumo':
+        novel = None
+    else:
+        raise Exception("Tipo não identificado")
+
+    assert isinstance(novel, Novel)
+    first_volume = 1
+    last_volume = 2
+    for i in range(first_volume, last_volume+1):
+        novel.add_chosen_volume(f"{i}")
+    novel.process()
--- utils.py
+++ utils.py
@@ -2,10 +2,9 @@ from time import sleep
 import requests
 
 
-BASE_URL = 'http://www.wuxiaworld.com'
 DELAY = 1
 
 
 def request_page(url):
     sleep(DELAY)
-    return requests.get(BASE_URL + url)
+    return requests.get(url)
