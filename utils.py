7056ede41c06543552508fd634d6aeab98a7b5ca
rodrigondec <rodrigondec@gmail.com>
2020-04-12 20:20:24 -0300

    user-agent with uuid

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+from uuid import uuid4
 from time import sleep
 
 import requests
@@ -8,4 +9,10 @@ DELAY = 1
 
 def request_page(url):
     sleep(DELAY)
-    return requests.get(url)
+
+    headers = {
+        'User-Agent': f'{uuid4()}',
+    }
+    request = requests.get(url, headers=headers)
+    request.raise_for_status()
+    return request
